093af425c39b8aea6361fe57bccea00f12ff55b1
Matthew Austin <85850088+mdaustin@users.noreply.github.com>
2023-02-17 12:01:13 -0500

    Initial StaticPages setup

---
+++ test/controllers/static_pages_controller_test.rb
@@ -0,0 +1,24 @@
+require "test_helper"
+
+class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @base_title = "My Movie Tier List"
+  end
+  
+  test "should get root" do
+    get root_url
+    assert_response :success
+  end
+  
+  test "should get home" do
+    get root_url
+    assert_response :success
+    assert_select "title", "Home | #{@base_title}"
+  end
+
+  test "should get help" do
+    get static_pages_help_url
+    assert_response :success
+    assert_select "title", "Help | #{@base_title}"
+  end
+end
