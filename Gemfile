98b4b5cf1da67cbfc3dfdece7cf7c0938334a469
Matthew Austin <85850088+mdaustin@users.noreply.github.com>
2023-02-18 01:35:22 -0500

    Setup minitests, and added 3 static pages

--- Gemfile
+++ Gemfile
@@ -72,4 +72,9 @@ group :test do
   gem "capybara"
   gem "selenium-webdriver"
   gem "webdrivers"
+  gem "rails-controller-testing"
+  gem "minitest"
+  gem "minitest-reporters"
+  gem "guard"
+  gem "guard-minitest"
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -17,8 +17,14 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get help" do
-    get static_pages_help_url
+    get help_url
     assert_response :success
     assert_select "title", "Help | #{@base_title}"
+  end
+
+  test "should get about" do
+    get about_url
+    assert_response :success
+    assert_select "title", "About | #{@base_title}"
   end
 end
