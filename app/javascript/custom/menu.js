8ff444d38c5906df8c2dc581dc9f44ff26fad9a1
Matthew Austin <85850088+mdaustin@users.noreply.github.com>
2023-03-01 17:59:43 -0500

    Mobile Menu added

---
+++ app/javascript/custom/menu.js
@@ -0,0 +1,20 @@
+/**
+ * 
+ * @param {*} selected_id 
+ * @param {*} menu_id 
+ * @param {*} toggle_class 
+ */
+function addToggleListener(selected_id, menu_id, toggle_class) {
+    let selected_element = document.querySelector(`#${selected_id}`);
+
+    selected_element.addEventListener("click", function(event) {
+        event.preventDefault();
+        let menu = document.querySelector(`#${menu_id}`)
+        menu.classList.toggle(toggle_class);
+    });
+}
+
+document.addEventListener("turbo:load", function() {
+    // ID, MENU, STATE
+    addToggleListener("mobile-button", "mobile-menu", "hidden")
+})
