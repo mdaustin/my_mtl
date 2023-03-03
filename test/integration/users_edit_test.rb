f59265c3c967f26523efca5615532e2d43691a55
Matthew Austin <85850088+mdaustin@users.noreply.github.com>
2023-03-03 17:25:00 -0500

    add user edit & user edit tests

---
+++ test/integration/users_edit_test.rb
@@ -0,0 +1,35 @@
+require "test_helper"
+
+class UsersEditTest < ActionDispatch::IntegrationTest
+  def setup 
+    @user = users(:matthew)
+  end
+
+  test "unsuccessful edit" do
+    log_in_as(@user)
+    get edit_user_path(@user)
+    assert_template 'users/edit'
+    patch user_path(@user), params: { user: { username: "",
+                                               email: "foo@invalid",
+                                               password: "foo",
+                                               password_confirmation: "bar" } }
+    assert_template 'users/edit'
+    assert_select 'div.bg-red-50'
+  end
+
+  test "successful edit" do 
+    get edit_user_path(@user)
+    assert_template 'users/edit'
+    username = 'foobar'
+    email = 'foo@bar.ca'
+    patch user_path(@user), params: { user: { username: username,
+                                              email: email, 
+                                              password: "",
+                                              password_confirmation: "" } }
+    assert_not flash.empty?
+    assert_redirected_to @user
+    @user.reload
+    assert_equal username, @user.username
+    assert_equal email, @user.email
+  end
+end
