4228176745a95aaf92ab2055fff03e9d2a0710aa
Matthew Austin <85850088+mdaustin@users.noreply.github.com>
2023-03-03 17:52:20 -0500

    Enabled friendly forwarding on users update

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -16,7 +16,10 @@ module SessionsHelper
     # Returns the current user
     def current_user
         if(user_id = session[:user_id])
-            @current_user ||= User.find_by(id: user_id)
+            user = User.find_by(id: user_id)
+            if user && session[:session_token] == user.session_token
+                @current_user = user
+            end
         elsif (user_id = cookies.encrypted[:user_id])
             user = User.find_by(id: user_id)
             if user && user.authenticated?(cookies[:remember_token])
@@ -24,6 +27,11 @@ module SessionsHelper
                 @current_user = user
             end
         end
+    end
+
+    # Returns true if the given user is the current user
+    def current_user?(user)
+        user && user == current_user
     end
 
     # Returns true if the user is logged in
@@ -43,5 +51,10 @@ module SessionsHelper
         forget(current_user)
         reset_session
         @current_user = nil
+    end
+
+    # Stores teh URL trying to be accessed
+    def store_location
+        session[:forwarding_url] = request.original_url if request.get?
     end
 end
