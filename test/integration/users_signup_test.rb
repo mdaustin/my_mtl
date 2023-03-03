f2bb1c8b75bda11215ce47d574ed742d078fde4c
Matthew Austin <85850088+mdaustin@users.noreply.github.com>
2023-03-03 13:28:59 -0500

    integration test for valid user signup with flash

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -14,4 +14,17 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
       assert_select 'div.bg-red-50'
       assert_select 'div.field_with_errors'
   end
+
+  test "valid signup information" do
+    get signup_path
+    assert_difference 'User.count' do
+      post users_path, params: { user: { username: "ExampleUser",
+                                         email: "user@example.com",
+                                         password: "foobar",
+                                         password_confirmation: "foobar" } }
+    end
+    follow_redirect!
+    assert_template 'users/show'
+    assert_not flash.empty?
+  end
 end
