c2307aabf6fd5a6832f5f31496d45c5c1411a348
Matthew Austin <85850088+mdaustin@users.noreply.github.com>
2023-03-04 19:44:57 -0500

    fixed failing test

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -11,7 +11,7 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
     assert_template 'users/index'
     assert_select 'div.pagination'
     User.paginate(page: 1).each do |user|
-      assert_select 'a[href=?]', user_path(user), text: user.name
+      assert_select 'a[href=?]', user_path(user), text: user.username
     end
   end
 end
