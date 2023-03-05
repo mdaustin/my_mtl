470ffd86354c1326a5a675636e1b8e85c2c880b7
Matthew Austin <85850088+mdaustin@users.noreply.github.com>
2023-03-04 22:23:41 -0500

    added logic to not show deactivated accounts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,11 +5,12 @@ class UsersController < ApplicationController
 
   def index
     #@users = User.all
-    @users = User.paginate(page: params[:page])
+    @users = User.where(activated: true).paginate(page: params[:page])
   end 
 
   def show
     @user = User.find(params[:id])
+    redirect_to root_url, status: see_other unless @user.activated?
   end
 
   def new
@@ -23,8 +24,6 @@ class UsersController < ApplicationController
       @user.send_activation_email
       flash[:notice] = "Please check your email to activate your account."
       redirect_to root_url
-
-
       # reset_session
       # log_in @user
       # flash[:success] = "Welcome to MyMTL!"
