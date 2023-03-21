44523cb1fa7de15d9fc0bc787acde8ee819866a7
Matthew Austin <85850088+mdaustin@users.noreply.github.com>
2023-03-21 01:50:36 -0400

    Basic TierList tests

---
+++ test/models/tier_list_test.rb
@@ -0,0 +1,28 @@
+require "test_helper"
+
+class TierListTest < ActiveSupport::TestCase
+  def setup
+    @user = users(:matthew)
+    @tier_list = TierList.new(title: "Lorem ipsum", user_id: @user.id)
+    @tier_list = @user.tier_lists.build(title: "Lorem ipsum", description: "#{@user.username}'s Movie Tier List")
+  end
+
+  test "should be valid" do
+    assert @tier_list.valid?
+  end
+
+  test "user id should be present" do
+    @tier_list.user_id = nil
+    assert_not @tier_list.valid?
+  end
+
+  test "title should be present" do 
+    @tier_list.title = nil
+    assert_not @tier_list.valid?
+  end
+
+  test "description should be present" do 
+    @tier_list.description = nil
+    assert_not @tier_list.valid?
+  end
+end
