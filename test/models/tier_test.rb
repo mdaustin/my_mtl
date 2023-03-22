b28c58bf6664e36dce65a20b67c4447aa06d22a3
Matthew Austin <85850088+mdaustin@users.noreply.github.com>
2023-03-22 09:49:56 -0400

    initial tier tests

--- test/models/tier_test.rb
+++ test/models/tier_test.rb
@@ -1,7 +1,31 @@
 require "test_helper"
 
 class TierTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup 
+    @tier = tiers(:one)
+    @user = users(:matthew)
+  end
+
+  test "should be valid" do
+    assert @tier.valid?
+  end
+
+  test "tier_list_id should be present" do
+    @tier.tier_list_id = nil
+    assert_not @tier.valid?
+  end
+
+  test "title should be present" do
+    @tier.title = " "
+    assert_not @tier.valid?
+  end
+
+  test "title should not be too long" do
+    @tier.title = "a" * 101
+    assert_not @tier.valid?
+  end
+
+  test "tier should be ordered by row_order" do
+    assert_equal @tier, @tier.tier_list.tiers.rank(:row_order).first
+  end
 end
