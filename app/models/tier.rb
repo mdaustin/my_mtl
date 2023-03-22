f441d48bef69b5355aed5bea7cfcd67d46782da3
Matthew Austin <85850088+mdaustin@users.noreply.github.com>
2023-03-22 09:51:09 -0400

    added ranked model to tiers

--- app/models/tier.rb
+++ app/models/tier.rb
@@ -1,3 +1,9 @@
 class Tier < ApplicationRecord
+  include RankedModel
   belongs_to :tier_list
+  validates :title, presence: true, length: { maximum: 100 }
+  validates :tier_list, presence: true
+
+  # Needed for ranked_model gem
+  ranks :row_order, with_same: :tier_list_id
 end
