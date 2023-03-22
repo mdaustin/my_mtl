09bc9e24503466d67b0ae806b6645ba0b4b71bf2
Matthew Austin <85850088+mdaustin@users.noreply.github.com>
2023-03-22 09:51:19 -0400

    added seed for tiers

--- db/seeds.rb
+++ db/seeds.rb
@@ -36,3 +36,11 @@ if TierList.count == 0
         user.tier_lists.create!(title: title, description: "#{user.username}'s #{user.tier_lists.count.ordinalize} tier list")
     end
 end
+
+if Tier.count == 0
+    tier_list = TierList.first
+    for i in 1..3 do
+        title = "#{i.ordinalize} Tier"
+        tier_list.tiers.create!(title: title)
+    end        
+end
