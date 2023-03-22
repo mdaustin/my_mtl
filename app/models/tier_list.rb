8b2025b5d9992ea3e5049db377a66f1161461895
Matthew Austin <85850088+mdaustin@users.noreply.github.com>
2023-03-22 09:50:52 -0400

    add has_many tiers relation

--- app/models/tier_list.rb
+++ app/models/tier_list.rb
@@ -1,6 +1,6 @@
 class TierList < ApplicationRecord
   belongs_to :user
-
+  has_many :tiers, dependent: :destroy
   validates :user_id, presence: true
   validates :title , presence: true, length: { maximum: 50 }
   validates :description, presence: true, length: { maximum: 160 }
