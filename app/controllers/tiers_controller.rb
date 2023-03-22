cdd867eedd00635c83c8e13f19384b1d52ddb186
Matthew Austin <85850088+mdaustin@users.noreply.github.com>
2023-03-22 19:19:11 -0400

    Added turbo form/error messages for tier

--- app/controllers/tiers_controller.rb
+++ app/controllers/tiers_controller.rb
@@ -1,39 +1,67 @@
 class TiersController < ApplicationController
     before_action :set_tier, only: [:show, :edit, :update, :destroy]
     before_action :set_tier_list, only: [:index, :new, :create, :edit, :update, :destroy]
+    before_action :logged_in_user, only: [:new, :create, :edit, :update, :destroy]
+    #before_action :correct_user, only: [:create, :edit, :update, :destroy]
+    
 
     def index 
         @tiers = @tier_list.tiers
     end 
 
     def new 
-
+        @tier = Tier.new
     end
+
+    # Post /tier_lists/:tier_list_id/tiers
     def create 
         @tier = @tier_list.tiers.build(tier_params)
-        if @tier.save
-            respond_to do |format|
-                format.turbo_stream { render turbo_stream: turbo_stream.append(:tiers, @tier) }
-                format.html { redirect_to tier_list_path(@tier_list) }
+        respond_to do |format|
+            if @tier.save
+                format.turbo_stream do 
+                    render turbo_stream: [
+                        turbo_stream.update('new_tier', partial: 'tiers/form', locals: { tier: Tier.new })
+                    ]
+                end
+                format.html { redirect_to user_tier_list_path(@current_user, @tier_list), notice: "Tier was successfully created." }
+            else
+                flash[:danger] = "A problem occured while creating your tier."
+                format.turbo_stream do 
+                    render turbo_stream: [
+                        turbo_stream.update('new_tier', partial: 'tiers/form', locals: { tier: @tier })
+                    ]
+                end
+                format.html { render 'new', status: :unprocessable_entity }
+                format.json { render json: @tier.errors, status: :unprocessable_entity }
             end
-        else
-            flash[:danger] = "Tier could not be created."
-            redirect_to tier_list_path(@tier_list)
         end
     end
+
 
     # Patch/Put /tier_lists/:tier_list_id/tiers/:id
     def update
         respond_to do |format|
             if @tier.update(tier_params)
                 format.turbo_stream { render turbo_stream: turbo_stream.replace(@tier) }
-                format.html { redirect_to tier_list_path(@tier_list) }
+                format.html { redirect_to user_tier_list_path(@current_user, @tier_list) }
             else
                 format.html { render :edit }
             end
         end
     end
 
+
+    # Delete /tier_lists/:tier_list_id/tiers/:id
+    def destroy
+        @tier.destroy
+        respond_to do |format|
+            format.turbo_stream { render turbo_stream: turbo_stream.remove(@tier) }
+            format.html { redirect_to user_tier_list_path(@current_user, @tier_list) }
+        end
+    end
+
+
+    # Used to sort a tier
     def sort 
         # debugger
         @tier = Tier.find(params[:id])
