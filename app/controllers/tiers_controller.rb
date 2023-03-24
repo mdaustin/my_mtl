1f33111a9c343603bdcf9b58b47248f994957441
Matthew Austin <85850088+mdaustin@users.noreply.github.com>
2023-03-24 19:46:23 -0400

    added turbo create

--- app/controllers/tiers_controller.rb
+++ app/controllers/tiers_controller.rb
@@ -2,7 +2,7 @@ class TiersController < ApplicationController
     before_action :set_tier, only: [:show, :edit, :update, :destroy]
     before_action :set_tier_list, only: [:index, :new, :create, :edit, :update, :destroy]
     before_action :logged_in_user, only: [:new, :create, :edit, :update, :destroy]
-    #before_action :correct_user, only: [:create, :edit, :update, :destroy]
+    before_action :correct_user, only: [:edit, :create, :update, :destroy]
     
 
     def index 
@@ -20,12 +20,16 @@ class TiersController < ApplicationController
             if @tier.save
                 format.turbo_stream do 
                     render turbo_stream: [
-                        turbo_stream.update('new_tier', partial: 'tiers/form', locals: { tier: Tier.new })
+                        # Turbo Stream to update the tier form
+                        turbo_stream.update('new_tier', partial: 'tiers/form', locals: { tier: Tier.new }),
+                        # Turbo Stream to update the tier list
+                        turbo_stream.append('tiers', partial: 'tiers/tier', locals: { tier: @tier })
                     ]
                 end
                 format.html { redirect_to user_tier_list_path(@current_user, @tier_list), notice: "Tier was successfully created." }
             else
                 flash[:danger] = "A problem occured while creating your tier."
+                # Turbo Stream to update the tier form errors
                 format.turbo_stream do 
                     render turbo_stream: [
                         turbo_stream.update('new_tier', partial: 'tiers/form', locals: { tier: @tier })
@@ -36,7 +40,6 @@ class TiersController < ApplicationController
             end
         end
     end
-
 
     # Patch/Put /tier_lists/:tier_list_id/tiers/:id
     def update
@@ -53,11 +56,10 @@ class TiersController < ApplicationController
 
     # Delete /tier_lists/:tier_list_id/tiers/:id
     def destroy
+        debugger
         @tier.destroy
-        respond_to do |format|
-            format.turbo_stream { render turbo_stream: turbo_stream.remove(@tier) }
-            format.html { redirect_to user_tier_list_path(@current_user, @tier_list) }
-        end
+        
+        redirect_to user_tier_list_path(@current_user, @tier_list), notice: "Tier was successfully deleted."
     end
 
 
@@ -85,5 +87,11 @@ class TiersController < ApplicationController
         # Sets the tier list
         def set_tier_list
             @tier_list = TierList.find(params[:tier_list_id])
+        end
+
+        # Verifies correct user by redirecting user 
+        def correct_user
+            @tier_list = current_user.tier_lists.find_by(id: params[:tier_list_id])
+            redirect_to current_user, status: :see_other if @tier_list.nil?
         end
 end
