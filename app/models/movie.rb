d75ac6d2590b30f1195c1262f8f8cf53d2149d2c
Matthew Austin <85850088+mdaustin@users.noreply.github.com>
2023-03-24 22:39:58 -0400

    tier_movie validations and posterurl method

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,8 +1,15 @@
 class Movie < ApplicationRecord
-    
+    has_many :tier_movies, dependent: :destroy
+    has_many :tiers, through: :tier_movies
+
     validates :title, presence: true
     validates :overview, presence: true
     validates :release_date, presence: true
     validates :tmdb_id, presence: true, uniqueness: true, numericality: { only_integer: true }
     validates :runtime, presence: true, numericality: { only_integer: true, greater_than: 0 }
+
+    # Returns the url for the movie's poster
+    def poster_url
+        "https://image.tmdb.org/t/p/w92#{poster_path}"
+    end       
 end
