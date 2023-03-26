c18b8a37800b3e45d17a80a9e2c565943d032594
Matthew Austin <85850088+mdaustin@users.noreply.github.com>
2023-03-26 02:10:42 -0400

    Added test for tier_movies destroy

--- test/controllers/tier_movies_controller_test.rb
+++ test/controllers/tier_movies_controller_test.rb
@@ -1,4 +1,15 @@
 require "test_helper"
 
 class TierMoviesControllerTest < ActionDispatch::IntegrationTest
+
+    test "should delete tier movie" do
+        tier_movie = tier_movies(:one)
+        assert_difference('TierMovie.count', -1) do
+            delete user_tier_list_tier_tier_movie_path(tier_movie.tier.tier_list.user,
+                                                       tier_movie.tier.tier_list,
+                                                       tier_movie.tier,
+                                                       tier_movie)
+        end
+        assert_redirected_to user_tier_list_path(:id => tier_movie.tier.tier_list)
+    end
 end
