101b6460ae064580b6c9138fabcbe8e925044441
Matthew Austin <85850088+mdaustin@users.noreply.github.com>
2023-03-29 21:38:52 -0400

    added create_movie_from_tmdb_id

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -6,4 +6,22 @@ module MoviesHelper
     def movie_poster(movie)
         "https://image.tmdb.org/t/p/w154#{movie.poster_path}"
     end
+
+    def create_movie_from_tmdb_id(tmdb_id)
+        # Fetch movie details from TMDB
+        movie_details = Tmdb::Movie.detail(tmdb_id)
+        # Create a new movie
+        movie = Movie.new(
+            tmdb_id: movie_details.id,
+            title: movie_details.title,
+            overview: movie_details.overview,
+            poster_path: movie_details.poster_path,
+            release_date: movie_details.release_date,
+            runtime: movie_details.runtime
+        )
+        # Save the movie
+        movie.save
+        # Return the movie
+        movie
+    end
 end
