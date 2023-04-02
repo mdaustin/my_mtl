9dd8df50f52e3758790ecf489d206219ef8c699d
Matthew Austin <85850088+mdaustin@users.noreply.github.com>
2023-04-01 20:20:43 -0400

    Added searching

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,8 +1,12 @@
 class MoviesController < ApplicationController
     def search 
+
         query = params[:query]
         if query.present?
-            @movies = Tmdb::Search.movie(query).results
+            # Creates movie objects if they don't exist
+            @movies = Movie.search(params)
+            # Does not create movie objects if they don't exist
+            # @movies = Tmdb::Search.movie(query).results
         else
             @movies = []
         end
@@ -10,6 +14,14 @@ class MoviesController < ApplicationController
         @user = User.find(params[:user_id])
         @tier_list = TierList.find(params[:tier_list_id])
         @tiers = @tier_list.tiers
+        @search_results_tier = @tier_list.search_results_tier
+
+        # Clear out the search results tier
+        @search_results_tier.tier_movies.destroy_all
+
+        @movies.each do |movie|
+            @search_results_tier.tier_movies.create(movie: movie)
+        end
 
         render 'tier_lists/show'
         # render user_tier_list_path(@user, @tier_list)
