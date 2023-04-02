7ed096ff059c0fbbfdedbbdcb0434b43765c92e5
Matthew Austin <85850088+mdaustin@users.noreply.github.com>
2023-04-01 20:23:21 -0400

    added search_results_tier

--- app/models/tier_list.rb
+++ app/models/tier_list.rb
@@ -4,4 +4,9 @@ class TierList < ApplicationRecord
   validates :user_id, presence: true
   validates :title , presence: true, length: { maximum: 50 }
   validates :description, presence: true, length: { maximum: 160 }
+
+  # Create a default tier for the search results
+  def search_results_tier
+    tiers.find_or_create_by(title: "Search Results")
+  end
 end
