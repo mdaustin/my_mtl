149745b53e6c7c58e653c848ab3702b9f22bb9a3
Matthew Austin <85850088+mdaustin@users.noreply.github.com>
2023-04-01 20:27:50 -0400

    added test to ensure SearchResultsTier is filled

--- test/controllers/movies_controller_test.rb
+++ test/controllers/movies_controller_test.rb
@@ -9,6 +9,7 @@ class MoviesControllerTest < ActionDispatch::IntegrationTest
   test "should return search results" do
     get search_movies_path(@user, @tier_list), params: { query: "The Dark Knight" }
     assert_response :success
+    assert @tier_list.search_results_tier.movies.count > 0
   end
   
 end
