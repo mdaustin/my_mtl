df67b592db786c7e955029b82bf267dc824b57c0
Matthew Austin <85850088+mdaustin@users.noreply.github.com>
2023-04-01 20:24:59 -0400

    added search results test

--- test/controllers/movies_controller_test.rb
+++ test/controllers/movies_controller_test.rb
@@ -1,7 +1,14 @@
 require "test_helper"
 
 class MoviesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:matthew)
+    @tier_list = tier_lists(:mymtl)
+  end
+
+  test "should return search results" do
+    get search_movies_path(@user, @tier_list), params: { query: "The Dark Knight" }
+    assert_response :success
+  end
+  
 end
