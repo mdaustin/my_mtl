d978a72cc105edfd9668f2e3fd86cc5d35bc97ee
Matthew Austin <Matthew.Astn@gmail.com>
2023-05-20 18:33:51 -0400

    Implemented follow/unfollow turbostreams

--- test/integration/following_test.rb
+++ test/integration/following_test.rb
@@ -8,6 +8,45 @@ class Following < ActionDispatch::IntegrationTest
   end
 end
 
+class FollowTest < Following 
+
+  test "Should follow a user the standard way" do
+    assert_difference "@user.following.count", 1 do
+      post relationships_path, params: { followed_id: @other.id }
+    end
+    assert_redirected_to @other
+  end
+
+  test "Should follow a user with hotwire" do
+    assert_difference "@user.following.count", 1 do
+      post relationships_path(format: :turbo_stream), params: { followed_id: @other.id }
+    end
+  end
+end
+
+class Unfollow < Following
+  def setup
+    super
+    @user.follow(@other)
+    @relationship = @user.active_relationships.find_by(followed_id: @other.id)
+  end
+end
+
+class UnfollowTest < Unfollow
+  test "Should unfollow a user the standard way" do
+    assert_difference "@user.following.count", -1 do
+      delete relationship_path(@relationship)
+    end
+    assert_response :see_other
+  end
+
+  test "Should unfollow a user with hotwire" do
+    assert_difference "@user.following.count", -1 do
+      delete relationship_path(@relationship, format: :turbo_stream)
+    end
+  end
+end
+
 class FollowingPagesTest < Following
   test "following page" do
     get following_user_path(@user)
