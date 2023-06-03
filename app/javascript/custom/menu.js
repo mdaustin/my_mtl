dac7f03672f6828403919157c03b4b969ad6cf45
Matthew Austin <Matthew.Astn@gmail.com>
2023-06-02 22:35:36 -0400

    Logic to toggle svg depending on open close btn

--- app/javascript/custom/menu.js
+++ app/javascript/custom/menu.js
@@ -6,11 +6,21 @@
  */
 function addToggleListener(selected_id, menu_id, toggle_class) {
     let selected_element = document.querySelector(`#${selected_id}`);
+    let btnIcons = [];
+
+    if (selected_element.querySelector("svg") != null) {
+        selected_element.querySelectorAll("svg").forEach((svg) => {
+            btnIcons.push(svg);
+        })
+    }
 
     selected_element.addEventListener("click", function(event) {
         event.preventDefault();
         let menu = document.querySelector(`#${menu_id}`)
         menu.classList.toggle(toggle_class);
+        btnIcons.forEach((svg) => {
+            svg.classList.toggle("hidden");
+        });
     });
 }
 
